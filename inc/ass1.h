f13c9603e55f40fd99cb2cd5543c6941448acaec
Joshua Lafleur <77811114+JoshLafleur@users.noreply.github.com>
2022-09-20 10:47:26 -0400

    Starting point of assignment 1 with documentation

---
+++ inc/ass1.h
@@ -0,0 +1,13 @@
+/**
+ * @file ass1.h
+ * @brief  Header file of the Programming Assignment 1 COEN346 Fall 2022
+ * @author Joshua Lafleur (josh.lafleur@outlook.com)
+ * @version 1.0
+ * @date 2022-09-20
+ *
+ * @note We certify that this work is our own work and meet's the faculties certification of originality.
+ */
+
+#pragma once
+
+
--- src/ass1.cpp
+++ src/ass1.cpp
@@ -1,7 +1,24 @@
+/**
+ * @file ass1.cpp
+ * @brief  Source code of the Programming Assignment 1 COEN346 Fall 2022
+ * @author Joshua Lafleur (josh.lafleur@outlook.com)
+ * @version 1.0
+ * @date 2022-09-20
+ *
+ * @note We certify that this work is our own work and meet's the faculties certification of originality.
+ */
+
+/******************************************************************************
+ *                             I N C L U D E S
+ ******************************************************************************/
+
 #include <iostream>
 #include "ass1.h"
 
-extern int test();
+
+/******************************************************************************
+ *                     P R I V A T E  F U N C T I O N S
+ ******************************************************************************/
 
 int main() {
     std::cout << "Hello World!";
