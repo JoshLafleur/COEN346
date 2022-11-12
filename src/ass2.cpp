36001dbf2d9cef5dea5d5769a0f61ad1bd532ff8
Joshua Lafleur <77811114+JoshLafleur@users.noreply.github.com>
2022-11-12 15:29:37 -0500

    temp before mem managed

--- src/ass2.cpp
+++ src/ass2.cpp
@@ -14,6 +14,7 @@
  ******************************************************************************/
 
 #include "ass2.h"
+#include <cstdio>
 #include <pthread.h>
 #include <sched.h>
 #include <signal.h>
@@ -38,6 +39,7 @@ typedef struct {
 typedef struct {
   string name;
   vector<proc_S> procs;
+  void* active_index;
 } user_S;
 
 typedef struct {
@@ -52,121 +54,22 @@ typedef struct {
  ******************************************************************************/
 
 static scheduler_S sched;
+static pthread_t sched_thread;
 const unsigned int *const quantum = &sched.quantum;
-unsigned int time_sec = ~0;
+unsigned int time_sec = 0;
 
 /******************************************************************************
  *          P R I V A T E  F U N C T I O N  P R O T O T Y P E S
  ******************************************************************************/
 
-static void* scheduler(void* arg);
+static void *scheduler(void *arg);
 static void *func(void *arg);
 static void scdl(int arg);
+static void thread_sig(int sig);
 
 /******************************************************************************
  *                     P R I V A T E  F U N C T I O N S
  ******************************************************************************/
-
-/**
- * @brief  Scheduling algorithm
- *
- * @param arg UNUSED
- */
-static void* scheduler(void* arg) {
-  UNUSED(arg);
-  static unsigned int last_start_time = 0;
-  static vector<user_S> active_users;
-  unsigned int user_quantum;
-
-  time_sec++;
-
-  if (last_start_time % sched.quantum == 0) {
-    vector<int> user_deletion_index;
-
-    for (size_t user = 0; user < sched.waiting_users.size(); user++) {
-      user_S tmp;
-      vector<int> proc_deletion_index;
-
-      tmp.name = sched.waiting_users[user].name;
-
-      for (size_t proc = 0; proc < sched.waiting_users[user].procs.size();
-           proc++) {
-        if (sched.waiting_users[user].procs[proc].data.state == FINISHED) {
-          proc_deletion_index.push_back(proc);
-          continue;
-        }
-        if (sched.waiting_users[user].procs[proc].start_time <= time_sec) {
-          tmp.procs.push_back(sched.waiting_users[user].procs[proc]);
-        }
-      }
-      while (proc_deletion_index.size() > 0) {
-        sched.waiting_users[user].procs.erase(
-            sched.waiting_users[user].procs.begin() +
-            proc_deletion_index.back());
-        proc_deletion_index.pop_back();
-      }
-
-      if (sched.waiting_users[user].procs.size() == 0) {
-        user_deletion_index.push_back(user);
-      }
-
-      if (tmp.procs.size() > 0)
-        active_users.push_back(tmp);
-    }
-
-    while (user_deletion_index.size() > 0) {
-      sched.waiting_users.erase(sched.waiting_users.begin() +
-                                user_deletion_index.back());
-      user_deletion_index.pop_back();
-    }
-
-    if (sched.waiting_users.size() == 0)
-      exit(0);
-
-    user_quantum = sched.quantum / active_users.size();
-
-    for (unsigned int user = 0; user < active_users.size(); user++) {
-      unsigned int proc_quantum =
-          user_quantum / active_users[user].procs.size();
-
-      for (unsigned int proc = 0; proc < active_users[user].procs.size();
-           proc++) {
-        active_users[user].procs[proc].run_time_quantum = proc_quantum;
-        active_users[user].procs[proc].running = false;
-      }
-    }
-
-    sched.curr_user = active_users.begin();
-    sched.curr_proc = sched.curr_user->procs.begin();
-    goto run;
-  } else {
-  run:
-    if (sched.curr_proc->data.state == UNSTARTED) {
-      pthread_create(&sched.curr_proc->thread, NULL, func,
-                     &sched.curr_proc->data);
-      sched.curr_proc->run_time_quantum--;
-      sched.curr_proc->running = true;
-      return NULL;
-    } else if (sched.curr_proc->running == false) {
-      sched.curr_proc->running = true;
-      sched.curr_proc->run_time_quantum--;
-      pthread_kill(sched.curr_proc->thread, SIGUSR2);
-      return NULL;
-    } else if (sched.curr_proc->run_time_quantum != 0) {
-      sched.curr_proc->run_time_quantum--;
-      return NULL;
-    } else {
-      sched.curr_proc->running = false;
-      pthread_kill(sched.curr_proc->thread, SIGUSR1);
-      if (sched.curr_proc++ == sched.curr_user->procs.end()) {
-        sched.curr_user++;
-        sched.curr_proc = sched.curr_user->procs.begin();
-        goto run;
-      }
-    }
-  }
-  return NULL;
-}
 
 static void thread_sig(int sig) {
   switch (sig) {
@@ -178,15 +81,202 @@ static void thread_sig(int sig) {
   }
 }
 
-void sched(int arg) {
+/**
+ * @brief  Scheduling algorithm
+ *
+ * @param arg UNUSED
+ */
+void *scheduler(void *arg) {
+  UNUSED(arg);
+  static unsigned int last_start_time = 0;
+  static vector<user_S> active_users;
+  unsigned int user_quantum;
+  cpu_set_t cpu_mask;
+  struct sigaction act1;
+  struct sigaction act2;
+  struct itimerval val;
 
+  CPU_ZERO(&cpu_mask);
+  CPU_SET(0, &cpu_mask);
+
+  sched_setaffinity(0, sizeof(cpu_mask), &cpu_mask);
+  
+  act1.sa_handler = thread_sig;
+  act1.sa_flags = 0;
+  sigemptyset(&act1.sa_mask);
+  sigaction(SIGUSR1, &act1, NULL);
+  sigaction(SIGUSR2, &act1, NULL);
+
+
+  act2.sa_handler = scdl;
+  act2.sa_flags = 0;
+  sigemptyset(&act2.sa_mask);
+  sigaction(SIGPROF, &act2, NULL);
+
+  val.it_value.tv_sec = 1;
+  val.it_value.tv_usec = 0;
+  val.it_interval = val.it_value;
+  setitimer(ITIMER_PROF, &val, NULL);
+
+  sched.curr_user = sched.waiting_users.end();
+
+  while (sched.waiting_users.size() || active_users.size()) {
+      vector<int> user_deletion_index;
+
+      if (((time_sec - last_start_time) % sched.quantum == 0) || (active_users.size() == 0)) {
+      if (sched.curr_user != sched.waiting_users.end())
+          if (sched.curr_proc->running == true) {
+            cout << "Time " << time_sec << ", User " << sched.curr_user->name << 
+                ", Process " << sched.curr_proc - sched.curr_user->procs.begin() <<
+                ", Paused\n";
+        sched.curr_proc->running = false;
+      }
+resched:
+      for (size_t user = 0; user < sched.waiting_users.size(); user++) {
+        user_S tmp;
+        vector<int> proc_deletion_index;
+
+        tmp.name = sched.waiting_users[user].name;
+
+        for (size_t proc = 0; proc < sched.waiting_users[user].procs.size();
+             proc++) {
+          if (sched.waiting_users[user].procs[proc].start_time <= time_sec) {
+            tmp.procs.push_back(sched.waiting_users[user].procs[proc]);
+            proc_deletion_index.push_back(proc);
+            
+          }
+        }
+        
+        if (tmp.procs.size() > 0) {
+          if (sched.waiting_users[user].active_index == 0) {
+            active_users.push_back(tmp);
+            sched.waiting_users[user].active_index = (void*) &active_users[active_users.size() - 1]; 
+          } else {
+              for (unsigned int i = 0; i < tmp.procs.size(); i++)
+                  ((user_S*) sched.waiting_users[user].active_index)->procs.push_back(tmp.procs[i]);
+          }
+        }
+        
+        while (proc_deletion_index.size() > 0) {
+          sched.waiting_users[user].procs.erase(
+              sched.waiting_users[user].procs.begin() +
+              proc_deletion_index.back());
+          proc_deletion_index.pop_back();
+        }
+
+        if (sched.waiting_users[user].procs.size() == 0) {
+          user_deletion_index.push_back(user);
+        }
+      }
+
+      while (user_deletion_index.size() > 0) {
+        sched.waiting_users.erase(sched.waiting_users.begin() +
+                                  user_deletion_index.back());
+        user_deletion_index.pop_back();
+      }
+
+      if ((sched.waiting_users.size() == 0) && (active_users.size() == 0))
+        exit(0);
+
+      if (active_users.size() != 0) {
+        user_quantum = sched.quantum / active_users.size();
+        goto cont;
+      }
+
+      goto skip;
+
+cont:
+      for (unsigned int user = 0; user < active_users.size(); user++) {
+        unsigned int proc_quantum =
+            user_quantum / active_users[user].procs.size();
+
+        for (unsigned int proc = 0; proc < active_users[user].procs.size();
+             proc++) {
+          active_users[user].procs[proc].run_time_quantum = proc_quantum;
+          active_users[user].procs[proc].running = false;
+        }
+      }
+
+      sched.curr_user = active_users.begin();
+      sched.curr_proc = sched.curr_user->procs.begin();
+      goto run;
+    } else {
+    run:
+      if (sched.curr_proc->data.state == UNSTARTED) {
+        cout << "Time " << time_sec << ", User " << sched.curr_user->name << 
+            ", Process " << sched.curr_proc - sched.curr_user->procs.begin() <<
+            ", Started\n";
+        cout << "Time " << time_sec << ", User " << sched.curr_user->name << 
+            ", Process " << sched.curr_proc - sched.curr_user->procs.begin() <<
+            ", Resumed\n";
+        last_start_time = time_sec;
+        sched.curr_proc->run_time_quantum--;
+        sched.curr_proc->running = true;
+        pthread_create(&sched.curr_proc->thread, NULL, func,
+                       &sched.curr_proc->data);
+      } else if (sched.curr_proc->running == false) {
+        cout << "Time " << time_sec << ", User " << sched.curr_user->name << 
+            ", Process " << sched.curr_proc - sched.curr_user->procs.begin() <<
+            ", Resumed\n";
+        last_start_time = time_sec;
+        sched.curr_proc->running = true;
+        sched.curr_proc->run_time_quantum--;
+        pthread_kill(sched.curr_proc->thread, SIGUSR2);
+      } else if (sched.curr_proc->data.state == FINISHED || sched.curr_proc->data.run_time == 0) {
+        cout << "Time " << time_sec << ", User " << sched.curr_user->name << 
+            ", Process " << sched.curr_proc - sched.curr_user->procs.begin() <<
+            ", Finished\n";
+        sched.curr_user->procs.erase(sched.curr_proc--);
+        if (sched.curr_user->procs.size() == 0) {
+          active_users.erase(sched.curr_user--);
+          if (active_users.size() == 0) goto finished;
+          sched.curr_proc = sched.curr_user->procs.begin();
+        }
+        goto done;
+      } else if (sched.curr_proc->run_time_quantum != 0) {
+        sched.curr_proc->run_time_quantum--;
+        pthread_kill(sched.curr_proc->thread, SIGUSR2);
+      } else {
+        cout << "Time " << time_sec << ", User " << sched.curr_user->name << 
+            ", Process " << sched.curr_proc - sched.curr_user->procs.begin() <<
+            ", Paused\n";
+        sched.curr_proc->running = false;
+        //pthread_kill(sched.curr_proc->thread, SIGUSR1);
+done:
+        if (++sched.curr_proc == sched.curr_user->procs.end()) {
+          if (++sched.curr_user == active_users.end())
+            goto resched;
+          sched.curr_proc = sched.curr_user->procs.begin();
+        }
+        goto run;
+      }
+    }
+skip:
+    pause();
+    time_sec++;
+  }
+
+finished:
+  return NULL;
 }
 
-static void *func(void *arg) {
-  thread_data_S *data = (thread_data_S *)arg;
-  struct sigaction act;
+void scdl(int arg) {
+    UNUSED(arg);
+    pthread_kill(sched_thread, SIGUSR2);
+    
+}
 
-  data->state = RUNNING;
+void *func(void *arg) {
+  thread_data_S data = *((thread_data_S *)arg);
+  struct sigaction act;
+  cpu_set_t cpu_mask;
+  
+  CPU_ZERO(&cpu_mask);
+  CPU_SET(0, &cpu_mask);
+
+  sched_setaffinity(0, sizeof(cpu_mask), &cpu_mask);
+
+  ((thread_data_S*)arg)->state = RUNNING;
 
   act.sa_handler = thread_sig;
   act.sa_flags = 0;
@@ -194,10 +284,13 @@ static void *func(void *arg) {
   sigaction(SIGUSR1, &act, NULL);
   sigaction(SIGUSR2, &act, NULL);
 
-  while (data->run_time--)
-    sleep(1);
-
-  data->state = FINISHED;
+  while (data.run_time) {
+      cout << data.run_time << endl;
+      data.run_time--;
+      pause();
+  }
+  ((thread_data_S*)arg)->state = FINISHED;
+  pthread_kill(sched_thread, SIGUSR2);
 
   pthread_exit(NULL);
 }
@@ -208,10 +301,6 @@ static void *func(void *arg) {
  * @retval   0
  */
 int main() {
-  struct sigaction act;
-  struct itimerval val;
-  cpu_set_t cpu_mask;
-
   fstream file_in = fstream("Input.txt");
   string tmp;
 
@@ -236,6 +325,7 @@ int main() {
       proc.func = (void *)func;
       proc.data.state = UNSTARTED;
       user.procs.push_back(proc);
+      user.active_index = 0;
     }
 
     sched.waiting_users.push_back(user);
@@ -243,22 +333,10 @@ int main() {
     tmp.clear();
   }
 
-  CPU_ZERO(&cpu_mask);
-  CPU_SET(0, &cpu_mask);
+  pthread_create(&sched_thread, NULL, scheduler,
+                 NULL);
 
-  sched_setaffinity(0, sizeof(cpu_mask), &cpu_mask);
+  while (1);
 
-  act.sa_handler = scheduler;
-  act.sa_flags = 0;
-  sigemptyset(&act.sa_mask);
-  sigaction(SIGPROF, &act, NULL);
-
-  val.it_value.tv_sec = 1;
-  val.it_value.tv_usec = 0;
-  val.it_interval = val.it_value;
-  setitimer(ITIMER_PROF, &val, NULL);
-
-  while (1)
-    ;
   return 0;
 }
